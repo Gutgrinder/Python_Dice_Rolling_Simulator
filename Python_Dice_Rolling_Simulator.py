1c9349c3975b7466459c9c834283100014bbd4f5
Gerrit Ohrner <ohrner90@yahoo.de>
2017-01-03 23:02:22 +0100

    Add files via upload

---
+++ Python_Dice_Rolling_Simulator.py
@@ -0,0 +1,8 @@
+﻿import random
+abfrage = 'j'
+while abfrage != 'n':
+	
+	MaxZahl = input('Wie viele Seiten hat der Wuerfel? ')
+	print('Der Wuerfel zeigt die Zahl: ', random.randint(1, int(MaxZahl)))
+	abfrage = input ('Willst du noch einmal Wuerflen? (j/n): ')
+
